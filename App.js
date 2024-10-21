198e5a51b20d25893b6dbd1c541135255520c21a
elena abbate <100029031+elenaAbbate@users.noreply.github.com>
2024-10-21 17:38:42 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import MapComponent from './components/MapComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hunger map</h1>
+      <MapComponent />
+    </div>
+  );
+};
+
+export default App;
